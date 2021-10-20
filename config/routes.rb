0c4fc5b1c5eeffcd4f5d67f4af4706224e83d0af
takaki-0611 <pq.earlyripe.earlyrotten@gmail.com>
2021-10-20 12:02:46 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+  devise_for :users
+  resources :users,only: [:show, :index, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+end
